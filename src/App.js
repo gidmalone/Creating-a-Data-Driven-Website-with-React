167ae2f0be9dd38e9d9636bf13cd13c01a5d3bae
Gid Malone <gideonmalone16@gmail.com>
2026-01-21 13:23:35 -0500

    UseEffect developed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [sites, setSites] = useState([])
+
+  useEffect(() => {
+    async function fetchData(){
+    const response = await fetch("/Project3Files/BoyleSites.json")
+      const result = await response.json()
+      setSites(result)
+  }
+  fetchData()
+}, [])
+
+  console.log(sites)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{sites.length}</p>
       </header>
     </div>
   );
